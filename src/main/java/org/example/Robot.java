e8f23f17fc7f35fc2c430cd07d417efc22284a48
Victoria Probst <victoria.probst13@gmail.com>
2022-07-01 14:47:08 -0400

    Completed creating Robot Class and Attack Function and Printed Result to Console.

---
+++ src/main/java/org/example/Robot.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Robot {
+
+
+
+    //Instance variables (HAS A)
+    public String name;
+    public String weaponName;
+    public int weaponPower;
+
+   //Constructor
+    public Robot() {
+        this.name = "Rodger";
+        this.weaponPower = 50;
+    }
+
+    public void Attack() {
+        System.out.println(this.name + " attacks with their " + this.weaponName + " for " + this.weaponPower + "damage");
+    }
+}
